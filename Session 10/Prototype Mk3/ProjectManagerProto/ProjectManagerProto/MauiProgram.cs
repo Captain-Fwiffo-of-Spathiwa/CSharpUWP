868df27416748507672659cc0a1bcced7dbbd561
Captain-Fwiffo-of-Spathiwa <44868706+Captain-Fwiffo-of-Spathiwa@users.noreply.github.com>
2026-05-30 16:07:54 +0800

    Add window positioning, fix scrolling list

--- Session 10/Prototype Mk3/ProjectManagerProto/ProjectManagerProto/MauiProgram.cs
+++ Session 10/Prototype Mk3/ProjectManagerProto/ProjectManagerProto/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.UI.Windowing;
+using Microsoft.Maui.LifecycleEvents;
+using Windows.Graphics;
 
 namespace ProjectManagerProto
 {
@@ -13,10 +18,35 @@ namespace ProjectManagerProto
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureLifecycleEvents(events =>
+                {
+#if WINDOWS
+                    events.AddWindows(windows =>
+                    {
+                        windows.OnWindowCreated(window =>
+                        {
+                            const int x = 20;
+                            const int y = 20;
+                            const int width = 800;
+                            const int height = 600;
+
+                            var mauiWinUIWindow = (Microsoft.UI.Xaml.Window)window;
+                            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(mauiWinUIWindow);
+                            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+                            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+                            if (appWindow is not null)
+                            {
+                                appWindow.MoveAndResize(new RectInt32(x, y, width, height));
+                            }
+                        });
+                    });
+#endif
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- Session 10/Prototype Mk3/ProjectManagerProto/ProjectManagerProto/Views/ProjectsMainPage.xaml.cs
+++ Session 10/Prototype Mk3/ProjectManagerProto/ProjectManagerProto/Views/ProjectsMainPage.xaml.cs
@@ -7,6 +7,19 @@ namespace ProjectManagerProto.Views
         public ProjectsMainPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("message\n\n\n\n\n\n\n");
+            Window.Destroying += OnMainWindowDestroying;
+
+        }
+
+        private void OnMainWindowDestroying(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n\n\n\n\n\n\nDest");
         }
     }
 }
